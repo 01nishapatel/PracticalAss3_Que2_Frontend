9be1e21d31d834b9859854b61900fb7d04ac1d33
nisha <patel.nisha0104@gmail.com>
2022-11-05 22:47:24 +0530

    deleted files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import{HttpClientModule } from '@angular/common/http';
-import { BookService } from './book.service';
+
 import { ProductAddComponent } from './product-add/product-add.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
@@ -24,7 +24,7 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [BookService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/book.ts
+++
@@ -1,6 +0,0 @@
-export default class Book {
-    _id: String="";
-    name: String="";
-    price: Number=0;
-    quantity: Number=0;
-}
